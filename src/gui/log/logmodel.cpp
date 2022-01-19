c60902141f85668d2c53225d850350d3571e4ec3
xavier2k6 <42386382+xavier2k6@users.noreply.github.com>
2022-01-19 22:55:55 +0000

    test cornflowerblue

--- src/gui/log/logmodel.cpp
+++ src/gui/log/logmodel.cpp
@@ -143,7 +143,7 @@ LogMessageModel::LogMessageModel(QObject *parent)
     , m_foregroundForMessageTypes
     {
         {Log::NORMAL, UIThemeManager::instance()->getColor(QLatin1String("Log.Normal"), QColorConstants::Svg::limegreen)},
-        {Log::INFO, UIThemeManager::instance()->getColor(QLatin1String("Log.Info"), QColorConstants::Svg::mediumorchid)},
+        {Log::INFO, UIThemeManager::instance()->getColor(QLatin1String("Log.Info"), QColorConstants::Svg::cornflowerblue)},
         {Log::WARNING, UIThemeManager::instance()->getColor(QLatin1String("Log.Warning"), QColorConstants::Svg::goldenrod)},
         {Log::CRITICAL, UIThemeManager::instance()->getColor(QLatin1String("Log.Critical"), QColorConstants::Svg::red)}
     }
