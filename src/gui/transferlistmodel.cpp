c3baef6415d0ebaf3485c4f20c15fe1ba50f694e
xavier2k6 <42386382+xavier2k6@users.noreply.github.com>
2022-01-31 00:09:50 +0000

    test

--- src/gui/transferlistmodel.cpp
+++ src/gui/transferlistmodel.cpp
@@ -710,9 +710,9 @@ QColor getDefaultColorByState(const BitTorrent::TorrentState state)
     case BitTorrent::TorrentState::DownloadingMetadata:
     case BitTorrent::TorrentState::ForcedDownloadingMetadata:
         if (!dark)
-            return {3QColorConstants::Svg::mediumseagreen}; // Forest Green
+            return {UIThemeManager::instance()->getColor(), QColorConstants::Svg::mediumseagreen}; // Forest Green
         else
-            return {QColorConstants::Svg::mediumseagreen}; // Lime Green
+            return {UIThemeManager::instance()->getColor(), QColorConstants::Svg::mediumseagreen}; // Lime Green
     case BitTorrent::TorrentState::StalledDownloading:
     case BitTorrent::TorrentState::StalledUploading:
         if (!dark)
