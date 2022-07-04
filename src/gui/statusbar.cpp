3f08308b85a913186631a372c0df2d8212ad6e2d
xavier2k6 <42386382+xavier2k6@users.noreply.github.com>
2022-07-04 13:12:35 +0100

    Use latest dependencies

--- src/gui/statusbar.cpp
+++ src/gui/statusbar.cpp
@@ -76,7 +76,7 @@ StatusBar::StatusBar(QWidget *parent)
     m_dlSpeedLbl->setFocusPolicy(Qt::NoFocus);
     m_dlSpeedLbl->setCursor(Qt::PointingHandCursor);
     m_dlSpeedLbl->setStyleSheet(u"text-align:left;"_s);
-    m_dlSpeedLbl->setMinimumWidth(200);
+    m_dlSpeedLbl->setFixedWidth(300);
 
     m_upSpeedLbl = new QPushButton(this);
     m_upSpeedLbl->setIcon(UIThemeManager::instance()->getIcon(u"upload"_s, u"seeding"_s));
@@ -85,7 +85,7 @@ StatusBar::StatusBar(QWidget *parent)
     m_upSpeedLbl->setFocusPolicy(Qt::NoFocus);
     m_upSpeedLbl->setCursor(Qt::PointingHandCursor);
     m_upSpeedLbl->setStyleSheet(u"text-align:left;"_s);
-    m_upSpeedLbl->setMinimumWidth(200);
+    m_upSpeedLbl->setFixedWidth(300);
 
     m_lastExternalIPsLbl = new QLabel(tr("External IP: N/A"));
     m_lastExternalIPsLbl->setSizePolicy(QSizePolicy::Maximum, QSizePolicy::Preferred);
